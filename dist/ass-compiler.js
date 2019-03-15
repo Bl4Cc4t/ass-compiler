f9080c7eba6a33caa6efe20719c007c2a08e6d55
Bl4Cc4t <kuron3ko@cock.li>
2019-03-15 22:53:52 +0100

    ayy

--- dist/ass-compiler.js
+++ dist/ass-compiler.js
@@ -4,31 +4,6 @@
   (factory((global.assCompiler = {})));
 }(this, (function (exports) { 'use strict';
 
-  function parseEffect(text) {
-    var param = text
-      .toLowerCase()
-      .trim()
-      .split(/\s*;\s*/);
-    if (param[0] === 'banner') {
-      return {
-        name: param[0],
-        delay: param[1] * 1 || 0,
-        leftToRight: param[2] * 1 || 0,
-        fadeAwayWidth: param[3] * 1 || 0,
-      };
-    }
-    if (/^scroll\s/.test(param[0])) {
-      return {
-        name: param[0],
-        y1: Math.min(param[1] * 1, param[2] * 1),
-        y2: Math.max(param[1] * 1, param[2] * 1),
-        delay: param[3] * 1 || 0,
-        fadeAwayHeight: param[4] * 1 || 0,
-      };
-    }
-    return null;
-  }
-
   function parseDrawing(text) {
     return text
       .toLowerCase()
@@ -222,7 +197,7 @@
           dia[fmt] = parseTime(fld);
           break;
         case 'Effect':
-          dia[fmt] = parseEffect(fld);
+          dia[fmt] = fld;
           break;
         case 'Text':
           dia[fmt] = parseText(fld);
