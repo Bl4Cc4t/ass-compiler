06b1f374e06cf07aa6ec1859707ce4443285d3b2
Bl4Cc4t <Bl4Cc4t@users.noreply.github.com>
2019-03-15 21:29:05 +0100

    revert the last change
    it was dumb

--- src/parser/time.js
+++ src/parser/time.js
@@ -1,4 +1,4 @@
 export function parseTime(time) {
-  const t = time.split(':')
-  return t[0] * 3600000 + t[1] * 60000 + t[2] * 1000
+  const t = time.split(':');
+  return t[0] * 3600 + t[1] * 60 + t[2] * 1;
 }
