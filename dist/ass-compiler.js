1f5377a6494ecb90c34529aabe0705b1883847f7
Bl4Cc4t <kuron3ko@cock.li>
2019-03-16 21:26:49 +0100

    reintroduce ms again smh

--- dist/ass-compiler.js
+++ dist/ass-compiler.js
@@ -170,7 +170,7 @@
 
   function parseTime(time) {
     var t = time.split(':');
-    return t[0] * 3600 + t[1] * 60 + t[2] * 1;
+    return t[0] * 3600000 + t[1] * 60000 + t[2] * 1000;
   }
 
   function parseDialogue(text, format, isComment) {
